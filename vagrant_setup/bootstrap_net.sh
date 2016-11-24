ae0369ab4cde099b89133bbd548d51e0cf21e5c3
elliot <postsneakernet@gmail.com>
2016-11-24 17:57:51 -0500

    Add .net core

--- vagrant_setup/bootstrap_net.sh
+++ vagrant_setup/bootstrap_net.sh
@@ -2,5 +2,5 @@
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 
-sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.0-preview2.1-003177
+sudo apt-get -qqy update
+sudo apt-get -qqy install dotnet-dev-1.0.0-preview2.1-003177
